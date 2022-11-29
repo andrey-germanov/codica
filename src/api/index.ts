167c17ce097eff9faed0f233ba9fe146334ae786
 german <german@MacBook-Air-Ksenia.local>
2022-11-29 22:35:29 +0200

    fixed conditions REACT_APP_API_NODE in api/index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,4 +2,4 @@ import mock from "./mock"
 import real from "./real"
 
 export const api = 
-process.env.REACT_APP_API_MODE === "real" ? real : mock;
+process.env.REACT_APP_API_MODE === "mock" ? mock : real;
